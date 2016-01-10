fad2c1a46f2476833db48e73486d9b1722797293
Lokesh <lokesh.gahlot@wellsfargo.com>
2016-01-10 15:34:50 -0800

    Get and Post method added Changes to be committed: modified:   server.js

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('api/blogs', function(req, res) {
+
+});
+
+app.post('api/blogs', function(req, res) {
+  Blog
+});
+
 var port = 3000;
 
 app.listen(port);
